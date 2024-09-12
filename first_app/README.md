adead784a23144519fcc66d9ea4237805edf88fd
Tuğba Nur Aslan <tugbanur.aslan7@gmail.com>
2024-09-12 22:50:12 +0300

    upload

--- first_app/README.md
+++ first_app/README.md
@@ -15,6 +15,45 @@ const: uygulamamızın çalışma zamanı performansını optimize etmesine yard
       - Text          **Widgets that display some text on the screen**
 
 
+
+## Wrap with center
+
 ![alt text](images/image-3.png)
 
 ![alt text](images/image-4.png)
+
+
+## Types
+
+Dart is a type-safe languıage ---> All values are of certain Types
+
+More than one Types is possible & common
+
+##### 'Hello World!'  --- String Object
+##### 29    ---  int num Object
+##### MaterialApp --- MaterialApp Widget Object
+
+
+### Widgets are Objects
+
+Widgets = Objects = Data Structures in Memory
+
+
+![alt text](images/image-5.png)
+![alt text](images/image-6.png)
+
+### Generic Types
+
+Generic typs are "flexible types" that "work together" with other Types
+
+![alt text](images/image-7.png)
+![alt text](images/image-8.png)
+
+
+![alt text](images/image-9.png)
+![alt text](images/image-10.png)
+
+
+
+![alt text](images/image-11.png)
+![alt text](images/image-12.png)
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main(){  // main, smzde işlev adıdır
+void main() {
+  // main, smzde işlev adıdır
 
-  runApp(MaterialApp(home: Scaffold(body: Center(child: Text('Hello World')))));   // sözde bir fonksiyondur
-                                                   // MaterialApp --> Flutter'ın sağladığı widget'lardan biri
-                        
-
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 60, 10, 148),
+                Color.fromARGB(255, 109, 23, 116),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World', style: TextStyle(
+              color: Colors.white,
+              fontSize: 28),
+              ),
+          ),
+        ),
+      ),
+    ),
+  ); // sözde bir fonksiyondur
+  // MaterialApp --> Flutter'ın sağladığı widget'lardan biri
 }
-
